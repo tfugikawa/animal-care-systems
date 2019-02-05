f6e7c700575711dd473cd3dbdcbad21203f31792
tfugikawa <47263300+tfugikawa@users.noreply.github.com>
2019-02-05 16:06:49 -0700

    Made to match ACS_V2.ino

--- arduino_code.ino
+++ arduino_code.ino
@@ -1,7 +1,7 @@
 // Global Vars
 unsigned long numSteps = 20000; // Microstepping
 unsigned int stepInterval = 100; // Used for delayMicroseconds, controls max speed (30 degrees per second)
-unsigned long varRate = 800;//62500;//Acceleration function used for rampup and rampdown (125000/2) This is a 125 ms frequency starting pulse (only moves 10 steps)
+unsigned int varRate = 800;//62500;//Acceleration function used for rampup and rampdown (125000/2) This is a 125 ms frequency starting pulse (only moves 10 steps)
 //const int accelRate = 2000; // Number of milliseconds for acceleration, used also for decceleration
 boolean eStop = false; // Logic for determining whether or not to stop
 boolean newData = false;
@@ -97,55 +97,41 @@ int getDir(){
 
 void moveCage(int numCages){
   //Code for moving the equivalent of one cage
-  //Use num steps
-  //call accel and deccel
-  //Serial.print(numCages + "\n");
-  //accel();
-  unsigned long var = numSteps*numCages/10;   //=numSteps*numCages*3 - 40...3* gear ratio - 40 steps form acceleration and deceleration
+  if (numCages > 0){
+  accel();
+  unsigned long var = 3*numSteps*numCages/10 - 5600;   //=numSteps*numCages*3 - 40...3* gear ratio - 40 steps form acceleration and deceleration
     for(unsigned long j=0;j<var;j++){ //40 steps form acceleration and deceleration
       digitalWrite(stepPin,HIGH);
       delayMicroseconds(stepInterval);// Used for delayMicroseconds, controls max speed
       digitalWrite(stepPin,LOW);
       delayMicroseconds(stepInterval);  
     }
-  //deccel();
+  deccel();
+  }
   return 0;
 }
 
 void accel(){
-  //accelRate = 2000000; // Number of microseconds for acceleration, used also for decceleration
-  unsigned long currentTime = micros();
-  while(varRate>100){
-    digitalWrite(stepPin,HIGH);
-    while((micros()-currentTime)<varRate){
-    // do not do anything during that period will need to eventually watch out for overflow error 
+  while(varRate > 100){
+    for (int i = 0;i<20;i++){
+      digitalWrite(stepPin,HIGH);
+      delayMicroseconds(varRate);// Used for delayMicroseconds, controls max speed
+      digitalWrite(stepPin,LOW);
+      delayMicroseconds(varRate);  
     }
-    currentTime = micros();
-    digitalWrite(stepPin,LOW);
-    while((micros()-currentTime)<varRate){
-      
-    }
-    Serial.print(varRate);
-    varRate-= 124820; //6240; // delay by 12480/2 microSeconds less each time (maximum to meet acceleration standards) 
-    currentTime = micros();
+    varRate-= 5;
   }
 }
 
 void deccel(){
-  //Inverse of acceleration function, used for decceleration only moves (10 steps)
-  unsigned long currentTime = micros();
-  while(varRate<62500){
-    digitalWrite(stepPin,HIGH);
-    while((micros()-currentTime)<varRate){
-    // do not do anything during that period will need to eventually watch out for overflow error 
+  while(varRate<800){
+    for (int i = 0;i<20;i++){
+      digitalWrite(stepPin,HIGH);
+      delayMicroseconds(varRate);// Used for delayMicroseconds, controls max speed
+      digitalWrite(stepPin,LOW);
+      delayMicroseconds(varRate);  
     }
-    currentTime = micros();
-    digitalWrite(stepPin,LOW);
-    while((micros()-currentTime)<varRate){
-      
-    }
-    varRate+= 12480; // delay by 33326/2 microSeconds less each time (maximum to meet acceleration standards);
-    currentTime = micros();
+    varRate+= 5; // delay by 33326/2 microSeconds less each time (maximum to meet acceleration standards);
   }
 }
 
